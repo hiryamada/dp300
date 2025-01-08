64d13d412b5995b06db926417b32230bcc4bf8be
hiryamada <71864722+hiryamada@users.noreply.github.com>
2025-01-08 19:54:07 +0900

    Update lab09.ps1

--- lab09.ps1
+++ lab09.ps1
@@ -1,2 +1,2 @@
 .\restore.ps1
-ssms -E -d AdventureWorks2017 lab09/lab09.sql
+& "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms" -E -d AdventureWorks2017 lab09/lab09.sql
